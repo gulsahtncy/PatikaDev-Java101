d44ec032d53652961449740370dda650e6a370dc
Gülşah <gulsahtuncay1@gmail.com>
2023-03-08 12:12:14 +0300

    Program to Calculate Area, Perimeter and Segment of a Circle

--- src/java101homeworks/D_TaximeterProgram.java
+++ src/java101homeworks/D_TaximeterProgram.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class D_TaximeterProgram {
 	public static void main(String[] args) {		
+		
 		int km;
 		double perKm = 2.20, total = 10, startPrice = 10;
 		
--- src/java101homeworks/E_DaireAlanCevreveDiliminiHesaplayanProgram.java
+++ src/java101homeworks/E_DaireAlanCevreveDiliminiHesaplayanProgram.java
@@ -2,29 +2,29 @@ package java101homeworks;
 
 import java.util.Scanner;
 
-public class E_DaireninAlanınıCevresiniveDaireDiliminiHesaplayanProgram {
+public class E_DaireAlanCevreveDiliminiHesaplayanProgram {
 	public static void main(String[] args) {
-		
+
 		int r, angle = 0;
 		double pi = 3.14;
 		angle = (angle < 361) ? angle : (angle % 360);
-				
+
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the radius of the circle : ");
 		r = input.nextInt();
-		System.out.println("Enter the angle of the circle segment : ");		
+		System.out.println("Enter the angle of the circle segment : ");
 		angle = input.nextInt();
-						
-		// Area =  π * r * r;
+
+		// Area = π * r * r;
 		// Perimeter = 2 * π * r;
 		// Segment = (pi * (r * r) * a) / 360
-		
+
 		double area = pi * r * r;
 		double perimeter = 2 * pi * r;
 		double segment = (pi * (r * r) * angle) / 360;
-		
+
 		System.out.println("Area of the circle : " + area);
-		System.out.println("Perimeter of the circle :" + perimeter);	
+		System.out.println("Perimeter of the circle :" + perimeter);
 		System.out.println("Area of the circle segment : " + segment);
-	}	
-}	
+	}
+}
