510a497dc3860d8f10846cee7ab7cba2cba9669f
Gülşah <gulsahtuncay1@gmail.com>
2023-03-10 17:03:00 +0300

    Calculator

--- src/java101homeworks/H_HesapMakinesi.java
+++ src/java101homeworks/H_HesapMakinesi.java
@@ -1,5 +1,46 @@
 package java101homeworks;
 
-public class H_HesapMakinesi {
+import java.util.Scanner;
 
+public class H_HesapMakinesi {
+	public static void main(String[] args) {
+		int number1, number2, select;
+
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the first number : ");
+		number1 = input.nextInt();
+		System.out.print("Enter the second number : ");
+		number2 = input.nextInt();
+
+		System.out.println("1 - Addition\n2 - Subtraction\n3 - Multiplication\n4 - Division ");
+		System.out.print("What is your choice? : ");
+		select = input.nextInt();
+
+		switch (select) {
+		case 1: {
+			System.out.println("Addition : " + (number1 + number2));
+			break;
+		}
+		case 2: {
+			System.out.println("Subtraction : " + (number1 - number2));
+			break;
+		}
+		case 3: {
+			System.out.println("Multiplication : " + (number1 * number2));
+			break;
+		}
+		case 4: {
+			switch (number2) {
+			case 0:
+				System.out.println("The number cannot be divided by 0.");
+				break;
+			default:
+				System.out.println("Division : " + (number1 / number2));
+			}	
+			break;
+		}
+		default:
+			System.out.println("Error : invalid status");
+		}
+	}
 }
