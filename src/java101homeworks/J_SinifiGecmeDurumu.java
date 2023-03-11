5caa6aa2f86d695e33b9ee7f08ff82a19794bc58
Gülşah <gulsahtuncay1@gmail.com>
2023-03-11 17:19:29 +0300

    Class Pass Status

---
+++ src/java101homeworks/J_SinifiGecmeDurumu.java
@@ -0,0 +1,60 @@
+package java101homeworks;
+
+import java.util.Scanner;
+
+public class J_SinifiGecmeDurumu {
+	public static void main(String[] args) {
+		int math, physics, turkish, chemistry, music;
+		double average, numberOfLessons = 0, classScores = 0;
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Math grade : ");
+		math = input.nextInt();
+		if (math >= 0 && math <= 100) {
+			classScores += math;
+			numberOfLessons++;
+		}
+
+		System.out.print("Physics grade : ");
+		physics = input.nextInt();
+		if (physics >= 0 && physics <= 100) {
+			classScores += physics;
+			numberOfLessons++;
+		}
+
+		System.out.print("Turkish grade : ");
+		turkish = input.nextInt();
+		if (turkish >= 0 && turkish <= 100) {
+			classScores += turkish;
+			numberOfLessons++;
+		}
+
+		System.out.print("Chemistry grade : ");
+		chemistry = input.nextInt();
+		if (chemistry >= 0 && chemistry <= 100) {
+			classScores += chemistry;
+			numberOfLessons++;
+		}
+
+		System.out.print("Music grade : ");
+		music = input.nextInt();
+		if (music >= 0 && music <= 100) {
+			classScores += music;
+			numberOfLessons++;
+		}
+
+		if (numberOfLessons != 0) {
+			average = (classScores / numberOfLessons);
+		} else {
+			average = 0;
+		}
+		System.out.println("Your average : " + average);
+
+		if (average <= 55) {
+			System.out.println("You failed the class.");
+		} else {
+			System.out.println("Congratulations, you passed the class.");
+		}
+	}
+}
